ea3e2e0f3780401bbfb5176998faf68e61538ffb
kollb <dermont@web.de>
2026-03-16 11:06:29 +0100

    fix: rename html file

--- test_standalone.py
+++ test_standalone.py
@@ -3,7 +3,7 @@ import os
 import re
 from playwright.sync_api import Page, expect
 
-BASE_URL = "http://localhost:8000/ho-tracker.html"
+BASE_URL = "http://localhost:8000/ho-planer.html"
 PRIVATE_DIR = "testfiles"
 
 # --- FIXTURES ---
